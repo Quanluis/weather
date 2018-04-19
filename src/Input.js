1108b45122d25facedfc96de8fe005bda33cf511
Luis Garzon <luisgarzon90@yahoo.com>
2018-04-19 16:08:52 -0400

    added onchange function that allows a user to input their suggested values instead of being stuck on intitial state.

--- src/Input.js
+++ src/Input.js
@@ -7,14 +7,27 @@ class Input extends React.Component {
       lat: 0,
       lon: 0
     };
+    this.onChangeLat = this.onChangeLat.bind(this);
+    this.onChangeLon = this.onChangeLon.bind(this);
+  }
+
+  onChangeLat(e) {
+    this.setState({
+      lat: +e.target.value
+    });
+  }
+  onChangeLon(e) {
+    this.setState({
+      lon: +e.target.value
+    });
   }
 
   render() {
     return (
       <div>
-        <input value={this.state.lat} type="number" />
+        <input min = {-90} max = {90} onChange = {(e) => this.onChangeLat(e) } value={this.state.lat} type="number" />
 
-        <input value={this.state.lon} val type="number" />
+        <input min = {-180} max = {180} onChange = {(e) => this.onChangeLon(e)} value={this.state.lon} val type="number" />
       </div>
     );
   }
