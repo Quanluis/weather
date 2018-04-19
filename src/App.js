704380333c1921fea288b07b3127da1021c79c9b
Luis Garzon <luisgarzon90@yahoo.com>
2018-04-19 17:50:29 -0400

    Installed axios for api calls as well as dotenv to hide api key from unwanted eyes.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import Input from './Input';
 import Submit from './Submit';
 import "./App.css";
 
+const axios = require('axios');
+
+axios.get('https://api.darksky.net/forecast/7c2d5e0d00caf2b29e7f1019936a897e/37.8267,-122.4233')
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+}); 
+
 class App extends Component {
+
+ 
+
   render() {
     return (
       <div>
