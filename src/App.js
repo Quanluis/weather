9590bcf2a320c16414f14fbafb1b71eb4c625658
Luis Garzon <luisgarzon90@yahoo.com>
2018-04-19 16:23:50 -0400

    Changed state in input component to empty string, created a simple submit button.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Input from './Input';
+import Submit from './Submit';
 import "./App.css";
 
 class App extends Component {
@@ -11,6 +12,8 @@ class App extends Component {
         </h1>
         <h2>Add your location!</h2>
         <Input />
+        <br/>
+        <Submit />
       </div>
     );
   }
